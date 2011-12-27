00849abe333161adb9f2c169fcc654feb7fbb25b
Dragos Stoenica <mindwar@gmail.com>
2011-12-27 05:16:46 +0200

    removed stuff

--- fashionscrape.php
+++ fashionscrape.php
@@ -86,8 +86,6 @@
 	curl_setopt($curl, CURLOPT_URL, $target_url);
 	$thehtml = curl_exec($curl);
 
-	/* @$html->loadHTMLFile($target_url); */
-
 	@$html->loadHTML($thehtml);
 	$xml = simplexml_import_dom($html);
 
